628a9e731cc59440df800237bde3ba66a9cced75
Jamie Lentin <jm@lentin.co.uk>
2019-08-19 11:37:05 +0100

    Site at 6c48515870087974fc6d8683372fea19ff76dbcc

---
+++ precache-manifest.1aa6634022413b9a337276ee349ea2f6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "665a0c29885dcdbb1747046006e4fa72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10d7d03ccbcd949ee8b6",
+    "url": "/static/css/main.d3f4ba94.chunk.css"
+  },
+  {
+    "revision": "c643578f5e271119dd86",
+    "url": "/static/js/2.59be25d3.chunk.js"
+  },
+  {
+    "revision": "10d7d03ccbcd949ee8b6",
+    "url": "/static/js/main.26f54ecb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "68ac2b35db8f5399da6c64b30825ae56",
+    "url": "/static/media/atm.68ac2b35.png"
+  },
+  {
+    "revision": "3a742f89008c70a4b7752d542ee7301e",
+    "url": "/static/media/community.3a742f89.svg"
+  },
+  {
+    "revision": "8ba21863628a4cf90eb364a39dce06e1",
+    "url": "/static/media/cryptocompare.8ba21863.jpg"
+  },
+  {
+    "revision": "4871cdc55f01fe55d0528dfa77645c40",
+    "url": "/static/media/digital-wallet.4871cdc5.png"
+  },
+  {
+    "revision": "0645e3e7d49f9bbbdbf18d5617a1b202",
+    "url": "/static/media/exch.0645e3e7.png"
+  },
+  {
+    "revision": "1251020d86f26960d68ccea71e63c79f",
+    "url": "/static/media/game.1251020d.svg"
+  },
+  {
+    "revision": "d9e29e2c1e74ee7670bad230d177a71f",
+    "url": "/static/media/kenya.d9e29e2c.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d736b840a13b41c539b730556579e653",
+    "url": "/static/media/newdonation-color.d736b840.svg"
+  },
+  {
+    "revision": "6cf5174a21c3f7b85f6a5f1f50946ff7",
+    "url": "/static/media/rocket.6cf5174a.svg"
+  },
+  {
+    "revision": "c8744fbb4693c309854139e888561992",
+    "url": "/static/media/search.c8744fbb.png"
+  },
+  {
+    "revision": "c6212cda15ea1ca27c85fa148673fead",
+    "url": "/static/media/smiley3.c6212cda.png"
+  },
+  {
+    "revision": "f5733b4388e10ec7d5cecfcc8220108d",
+    "url": "/static/media/tutor-web.f5733b43.png"
+  },
+  {
+    "revision": "7406e1879b5b455529b9d6a5daa14c9e",
+    "url": "/static/media/withwork-color.7406e187.svg"
+  }
+]);
