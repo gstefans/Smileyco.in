69cdfaa66bed8595ad34c61a44f9492d4371760f
Gunnar Stefansson <gstefans@gmail.com>
2019-12-04 12:45:49 +0000

    Added more about the SmileyCoin Fund

--- src/components/index-coin/IndexCoin.js
+++ src/components/index-coin/IndexCoin.js
@@ -18,8 +18,15 @@ export default class IndexCoin extends Component {
             </div>
             <div className = "coin__content__text">
                 <p>
-                The tutor-web project pre-mined just under 50% of all coins and since the initial phase, mining has been open to any miner. <br></br>
-                The pre-mined coins are used to reward students for their performance. In addition, the project will award grants to developers and entrepreneurs, who aim to use the Smileycoin in their systems or otherwise develop environments which lead to increased use of the SMLY.
+                The SmileyCoin is a linked to several projects. 
+                <br></br>
+                The <b>SmileyCoin Fund</b> is a fund to support educational efforts. 
+                The tutor-web rewards students for their performance using grants from the SmileyCoin Fund. 
+                In addition, the Fund will award grants to developers and entrepreneurs, who aim to use the Smileycoin in their systems or otherwise develop environments which lead to increased use of the SMLY.
+                <br></br>
+                Education in a Suitcase is a project run by the Smiley Charity, which receives automatic donations from SMLY miners. Some of these donations are paid forward. Recipients include UNICEF and the African Maths Initiative.
+                <br></br>
+                
                 </p>
             </div>
           </div>
